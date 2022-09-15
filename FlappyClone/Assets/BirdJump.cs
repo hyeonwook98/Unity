9851ea76471a3ceb879ea9a81216f4fdd6e75f9a
hyeonwook98 <hyeonwook98@naver.com>
2022-09-15 09:58:17 +0900

    chore: FlappyClone 게임 개발

---
+++ FlappyClone/Assets/BirdJump.cs
@@ -0,0 +1,34 @@
+using System.Collections; //외부 모듈을 가져오는 것 using = import
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdJump : MonoBehaviour // class 정의 , 상속받은 클래스 : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play();
+            rb.velocity = Vector2.up * jumpPower;// (0,3)
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        if(Score.score > Score.bestScore)
+        {
+            Score.bestScore = Score.score;
+        }
+        SceneManager.LoadScene("GameOverScene");
+    }
+}
---
+++ FlappyClone/Assets/MakePipe.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(3, Random.Range(-0.6f, 4.3f), 0);
+            timer = 0;
+            Destroy(newpipe, 10.0f);
+        }
+       
+    }
+}
